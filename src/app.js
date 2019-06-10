79cc8ca6d05212d2b3687fa6b37670615a8ea07c
Scott Bowler <scott.bowler@learningfuze.com>
2019-06-10 16:24:25 -0700

    Completed working version of code for session 5

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Greeting from './greeting';
+
+export default props => (
+    <div>
+        <h1>Welcome to React</h1>
+
+        <Greeting name="Kathy"/>
+        <Greeting name="James"/>
+    </div>
+);
